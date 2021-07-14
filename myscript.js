1541866e512bb68cb99e3a12ca30c87bb100c472
Maria Baia <mfb171201@gmail.com>
2021-07-14 11:59:27 -0500

    entrega 18

---
+++ myscript.js
@@ -0,0 +1,42 @@
+const button = document.getElementById("countButton");
+button.addEventListener("click", function() {  
+   // teu código vai aqui ... 
+   let typedText = document.getElementById("textInput").value;
+   typedText = typedText.toLowerCase();
+   typedText = typedText.replace(/[^a-z'\s]+/g, "");
+   const letterCounts = {};
+   for (let i = 0; i < typedText.length; i++) {
+        currentLetter = typedText[i];
+        // faça algo com cada letra 
+        if (letterCounts[currentLetter] === undefined) {
+            letterCounts[currentLetter] = 1; 
+        } else { 
+            letterCounts[currentLetter]++; 
+        }
+    }
+    for (let letter in letterCounts) { 
+        const span = document.createElement("span"); 
+        const textContent = `"${letter}": ${letterCounts[letter]}, `;
+        span.innerText = textContent; 
+        const letters = document.getElementById("lettersDiv");
+        letters.appendChild(span); 
+    }
+    words = typedText.split(/\s/);
+    const wordsCounts = {};
+    for (let i = 0; i < words.length; i++) {
+        currentWords = words[i];
+        // faça algo com cada letra 
+        if (wordsCounts[currentWords] === undefined) {
+            wordsCounts[currentWords] = 1; 
+        } else { 
+            wordsCounts[currentWords]++; 
+        }
+    }
+    for (let word in wordsCounts) { 
+        const span = document.createElement("span"); 
+        const textContent = `"${word}": ${wordsCounts[word]}, `;
+        span.innerText = textContent; 
+        const words = document.getElementById("wordsDiv");
+        words.appendChild(span); 
+    }
+});
